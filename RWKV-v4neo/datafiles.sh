9aa9dc81e9f8af8af07228098d88b38ad54cd333
Will Choe <jazzbearz@gmail.co>
2023-04-13 00:25:06 +0900

    scripts

--- RWKV-v4neo/datafiles.sh
+++ RWKV-v4neo/datafiles.sh
@@ -1,3 +1,4 @@
 # /bin/bash
-wget https: // huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/RWKV-4-Raven-3B-v9-Eng99 % 25-Other1 % 25-20230411-ctx4096.pth
+wget https://huggingface.co/BlinkDL/rwkv-4-raven/resolve/main/RWKV-4-Raven-3B-v9-Eng99%25-Other1%25-20230411-ctx4096.pth
 mv RWKV-4-Raven-3B-v9-Eng99%-Other1%-20230411-ctx4096.pth RWKV-4-Raven-3B-v9-EngOther-20230411-ctx4096.pth
+# mv ../../train.txt .
--- RWKV-v4neo/installpip.sh
+++ RWKV-v4neo/installpip.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
-pip install pytorch-lightning==1.9 deepspeed==0.7.0 
+pip install pytorch-lightning==1.9 deepspeed==0.7.0 transformers
