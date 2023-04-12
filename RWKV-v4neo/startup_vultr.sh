c909d8e15c750061bc66d93e167719b7fd6f7257
Will Choe <will@dooub.com>
2023-04-13 00:06:03 +0900

    script

---
+++ RWKV-v4neo/startup_vultr.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_515.43.04_linux.run
+sudo sh cuda_11.7.0_515.43.04_linux.run
+sudo apt-get install -y python-is-python3 python3 pip
+sudo pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+sudo pip install pytorch-lightning==1.9 deepspeed==0.7.0 
